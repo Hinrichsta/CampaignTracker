aa797bcc185ab2de9219737311ffeb3569eb89c9
tyler.hinrichs <tyler.hinrichs@bosselman.com>
2024-11-25 14:24:54 -0600

    Still working on Permissions

--- CampaignAPI/permissions.py
+++ CampaignAPI/permissions.py
@@ -13,8 +13,8 @@ class IsCampaignOwner(permissions.BasePermission):
     Custom permission to only allow owners of an object to view it.
     """
     def has_object_permission(self, request, view, obj):
-        if (request.user in CampaignUsers.user):
-            if (obj.CampaignCore == CampaignUsers.campaign) and (CampaignUsers.role == 'O'):
+        if (request.user == CampaignUsers.user) and (CampaignUsers.role == 'O'):
+            if (obj.campaign == CampaignUsers.campaign) or (obj.id == CampaignUsers.campaign):
                 return True
         return False
     
@@ -23,18 +23,24 @@ class IsCampaignAdmin(permissions.BasePermission):
     Custom permission to only allow owners of an object to view it.
     """
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        if (request.user in CampaignUsers.user) and (obj.campaign == CampaignUsers.campaign) and (CampaignUsers.role == 'A'):
+            return True
+        return False
     
 class IsCampaignUser(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to view it.
     """
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        if (request.user in CampaignUsers.user) and (obj.campaign == CampaignUsers.campaign) and (CampaignUsers.role == 'P'):
+            return True
+        return False
     
 class IsCampaignViewer(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to view it.
     """
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        if (request.user in CampaignUsers.user) and (obj.campaign == CampaignUsers.campaign) and (CampaignUsers.role == 'V'):
+            return True
+        return False
