1c435a36410ee30cc7eb0d6d3c767e01ba0f1d26
Hinrichsta <Hinrichstylera@gmail.com>
2024-12-07 17:35:51 -0600

    Started Front End...

--- Campaign-Ember/app/router.js
+++ Campaign-Ember/app/router.js
@@ -6,4 +6,18 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('login');
+  this.route('home');
+  this.route('campaignhome', { path: '/campaign/:campaign_id' });
+  this.route('income', { path: '/campaign/:campaign_id/income' });
+  this.route('payments', { path: '/campaign/:campaign_id/payment' });
+  this.route('items', { path: '/campaign/:campaign_id/items' });
+  this.route('vehicles', { path: '/campaign/:campaign_id/Vehicles' });
+  this.route('hirelings', { path: '/campaign/:campaign_id/Hirelings' });
+  this.route('party', { path: '/campaign/:campaign_id/party' });
+  this.route('calendar', { path: '/campaign/:campaign_id/calendar' });
+  this.route('about');
+  this.route('profile');
+  this.route('settings');
+});
