fd4bf76a812e7e9b3b21c4715b6d1390c792f758
Hinrichsta <Hinrichstylera@gmail.com>
2024-12-11 22:16:37 -0600

    Remove Ember

--- Campaign-Ember/app/adapters/application.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-import { computed } from '@ember/object';
-
-export default DS.RESTAdapter.extend({
-  host: computed(function () {
-    return 'http://localhost:8000';
-  }),
-  namespace: 'api/v1',
-});
--- Campaign-Ember/app/models/campaigncore.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-    id: DS.attr(),
-    campaign_name: DS.attr(),
-    description: DS.attr(),
-    public: DS.attr()
-});
