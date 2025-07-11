9f84d81bf6be466c5329db7aa1904aa29da6bfc5
Tyler A. Hinrichs <hinrichstylera@gmail.com>
2025-07-11 15:20:57 -0500

    refactor core folder

--- Campaign-Django/campaign_django/settings.py
+++ Campaign-Django/campaign_django/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-ROOT_URLCONF = 'Campaign-Django.urls'
+ROOT_URLCONF = 'campaign_django.urls'
 
 TEMPLATES = [
     {
@@ -82,7 +82,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Campaign-Django.wsgi.application'
+WSGI_APPLICATION = 'campaign_django.wsgi.application'
 
 
 # Database
